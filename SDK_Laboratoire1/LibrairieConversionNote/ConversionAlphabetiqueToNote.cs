32e13243bbe5d16bc59b086b03359c381363357c
Anthoni711 <aph.generation@gmail.com>
2022-07-19 16:01:46 -0400

    Toute
    Toute

---
+++ SDK_Laboratoire1/LibrairieConversionNote/ConversionAlphabetiqueToNote.cs
@@ -0,0 +1,25 @@
+﻿namespace LibrairieConversionNote
+{
+    public class ConversionAlphabetiqueToNote
+    {
+        public string Transformer(string alphabet)
+        {
+
+
+            switch (alphabet)
+            {
+                case "A": case "a": return "LA"; break;
+                case "B": case "b": return "SI"; break;
+                case "C": case "c": return "DO"; break;
+                case "D": case "d": return "RE"; break;
+                case "E": case "e": return "MI"; break;
+                case "F": case "f": return "FA"; break;
+                case "G": case "g": return "SOL"; break;
+                default: return string.Empty;
+            }
+
+        }
+
+
+    }
+}
--- SDK_Laboratoire1/SDK_Laboratoire1/Program.cs
+++ SDK_Laboratoire1/SDK_Laboratoire1/Program.cs
@@ -1,1 +1,35 @@
-﻿//pret pour repo
+﻿using LibrairieConversionNote;
+
+static class Program
+{
+    static int Main()
+    {
+        var converter = new ConversionAlphabetiqueToNote();
+       // converter.Transformer('P');
+       // Console.WriteLine(converter.Transformer('P'));
+
+        //On roule l'application non-stop, tant que CTRL+C
+        while (true)
+        {
+            Console.Write("Type de conversion à faire : ");
+            var input = Console.ReadLine();
+
+            //Si l'utilisateur tape note
+            if (input?.ToUpperInvariant() == "NOTE")
+            {
+                //On demande en boucle la coBnversion 
+                while (true)
+                {
+                    Console.Write("Conversion à faire : ");
+                    var note = Console.ReadLine();
+                    var result = converter.Transformer(note);
+                    string noteRetour = result == "" ? "Je ne suis pas implémentée" : result; 
+                    Console.Write(noteRetour);
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        return 0;
+    }
+}
